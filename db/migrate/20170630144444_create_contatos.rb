68d723dd44e751e96c0fa55121fea0eb2e02d939
Erik R. de Souza <erikikoo@hotmail.com>
2017-07-25 12:00:45 -0300

    'UpdatePagWelcome'

--- db/migrate/20170630144444_create_contatos.rb
+++ db/migrate/20170630144444_create_contatos.rb
@@ -2,7 +2,7 @@ class CreateContatos < ActiveRecord::Migration[5.0]
   def change
     create_table :contatos do |t|
       t.string :numero, :limit => 15
-      t.string :whatsapp
+      t.boolean :whatsapp, default: false
       t.references :cliente, foreign_key: true
 
       t.timestamps
