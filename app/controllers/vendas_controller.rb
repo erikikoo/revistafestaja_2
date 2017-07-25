866a98ec8d12ee39395269a5624a55e20652a4ac
Erik R. de Souza <erikikoo@hotmail.com>
2017-07-25 14:58:28 -0300

    'Update'

--- app/controllers/vendas_controller.rb
+++ app/controllers/vendas_controller.rb
@@ -8,7 +8,7 @@ class VendasController < ApplicationController
 
   before_action :authenticate_user!
   
-  require ('#{Rails.root}/lib/gera_codigo_venda.rb')
+  require '#{Rails.root}/lib/gera_codigo_venda.rb'
 
   # GET /vendas
   # GET /vendas.json
