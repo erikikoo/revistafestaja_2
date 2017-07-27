b564e5e426101964b85117b68ae4e0c95704e9ef
Erik R. de Souza <erikikoo@hotmail.com>
2017-07-27 15:53:29 -0300

    'RepairSeed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'ffaker'
+#require 'ffaker'
 
 User.create :email => "erikikoo@hotmail.com", :password => "HayHelena"
 User.create :email => "seudesigner@outlook.com.br", :password => "123456"
